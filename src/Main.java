99ed6d1fb1b91e3e7527bf437a7e455aa428dcb0
CesarFrutosX <cesarfrutos_x@yahoo.es>
2022-01-12 16:16:09 +0100

    Arreglo

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HOLA MMMMMMMMMMMMMMMMMMMMUNDO");
         System.out.println("HOLA 2");
+        System.out.println("Arreglo2");
     }
 }
