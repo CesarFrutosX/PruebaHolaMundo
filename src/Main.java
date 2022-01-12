4bfeba65cb2b1732af1d471fa76f497a7313f299
Francisco <86533117+Francisco-BM@users.noreply.github.com>
2022-01-12 16:44:26 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HOLA MMMMMMMMMMMMMMMMMMMMUNDO");
+        System.out.println("HOLa soy paco");
         System.out.println("HOLA 2");
         System.out.println("Arreglo2");
     }
